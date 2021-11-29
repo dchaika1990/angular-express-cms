cc7f96084d6f41fe6186a97d1db9e51892dd8f93
chaika <dchaika@inbox.ru>
2021-11-29 22:21:35 +0200

    create models

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const router = require('./routes');
 const app = express();
 
+app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(require('cors')());
 app.use('/api', router);
 
 module.exports = app;
