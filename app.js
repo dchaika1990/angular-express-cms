37fdb7378464ee6696ecb3aedc686bbda0522d73
chaika <dchaika@inbox.ru>
2021-11-29 22:45:38 +0200

    add mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require('./routes');
+const { MONGO_URI } = require('./config/keys');
 const app = express();
+
+mongoose
+	.connect(MONGO_URI)
+	.then(() => {
+		console.log('MongoDB connected.');
+	})
+	.catch((e) => {
+		console.log(e);
+	});
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	MONGO_URI:
+		'mongodb+srv://root:root@fullstack.czksx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+};
